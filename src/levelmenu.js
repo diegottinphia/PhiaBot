4b7ab43c800d4832cd7a742243d705009cbc2dc7
Genau Mein Ding <84249632+diegottinphia@users.noreply.github.com>
2021-08-24 13:46:09 +0200

    Update levelmenu.js

--- src/levelmenu.js
+++ src/levelmenu.js
@@ -26,7 +26,7 @@ PARA VER TU XP EN TIEMPO REAL
     
 *Recuerda que si actualizas tu bot o borras el numero del bot o otra cosa todos los levels de los particiantes quedaran en 0*
     
-ву ѕнαη∂υу
+ву 𝕯ͥ𝖎𝖊𝕲𝖔̈𝖙𝖙𝖎𝖓𝕻𝖍𝖎ͣ𝖆ͫ
 `
 
 }
